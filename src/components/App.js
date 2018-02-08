938360ba7898a77835bcc6937e45b7669b6e60a1
barks1212 <willbarkerwork@gmail.com>
2018-02-08 10:18:37 +0000

    basic setup, server listening

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return(
+      <section className="main">
+      <p>yo</p>
+      </section>
+    )
+  }
+}
+
+export default App;
