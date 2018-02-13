623e5485470b89d7b20f23dc47f7c3efea81823e
barks1212 <willbarkerwork@gmail.com>
2018-02-13 11:36:04 +0000

    rendered user articles on user profile page

--- src/components/User-articles.js
+++ src/components/User-articles.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import PT from 'prop-types';
 import { Link } from 'react-router-dom';
+import Voter from './Voter';
 
-const UserArticles = (props) => (
-  <section>
-    <p>yo</p>
-  </section>
-)
+class UserArticles extends React.Component {
+  render() {
+    return (
+      <section className="userArticles">
+        {this.props.articles.map((article, i) => {
+          return (
+            <section className="card" key={i}>
+              <section className="card-content">
+                <Voter article={article} updateArticleVote={this.updateArticleVote} />
+                <section className="cardBody">
+                  <strong><Link to={`/articles/${article._id}`}>{article.title}</Link></strong>
+                  <p>Submitted by <Link to={`/users/${article.created_by}`}>{article.created_by}</Link> to <Link to={`/topics/${article.belongs_to}/articles`}> {article.belongs_to}</Link></p>
+                  <p>{article.comments} comments</p>
+                </section>
+              </section>
+            </section>
+          )
+        })}
+      </section>
+    )
+  }
+  updateArticleVote = (articleId, vote) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/articles/${articleId}?${vote}`, {
+      method: 'PUT'
+    })
+      .then((res) => {
+        return res.json()
+      })
+  }
+}
+
+UserArticles.propTypes = {
+  articles: PT.array
+}
+
+
 
 export default UserArticles
