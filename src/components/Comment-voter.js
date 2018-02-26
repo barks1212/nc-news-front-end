1b671c811c9976f5d7241b433430a402bb4a2d55
barks1212 <willbarkerwork@gmail.com>
2018-02-26 16:49:55 +0000

    format comment poster overlay for mobile

---
+++ src/components/Comment-voter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PT from 'prop-types';
+
+class CommentVoter extends React.Component {
+  render () {
+    return (
+
+    )
+  }
+}
+
+export default CommentVoter;
